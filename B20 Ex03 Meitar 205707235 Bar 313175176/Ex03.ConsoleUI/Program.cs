b8b8b34dfa2a9364a73c38036b40790706110b89
MeitarBach <meitar.bach666@gmail.com>
2020-06-11 20:27:13 +0300

    Vehicle Test

--- B20 Ex03 Meitar 205707235 Bar 313175176/Ex03.ConsoleUI/Program.cs
+++ B20 Ex03 Meitar 205707235 Bar 313175176/Ex03.ConsoleUI/Program.cs
@@ -7,14 +7,22 @@ namespace Ex03.ConsoleUI
     {
         public static void Main()
         {
+            //// Vehicle TEST
+            Engine fuelEngine = new FuelEngine(eFuelType.Octane95, 20, 100);
+            Vehicle vehicle = new Vehicle("1234", "Ibiza", fuelEngine);
+            vehicle.WheelsList.Add(new Wheel("Michelin", 29, 32));
+            vehicle.WheelsList.Add(new Wheel("Michelin", 26, 32));
+            vehicle.WheelsList.Add(new Wheel("BarTires", 20, 27));
+            VehicleOwner bar = new VehicleOwner("bar", "0546");
+            vehicle.Owner = bar;
+            Console.WriteLine(vehicle);
+            Console.ReadLine();
+
+
+
             Wheel wheel = new Wheel("Michelin", 28, 30);
-            
-            //// Vehicle Test
-            
-
-
             //// Owner Test
-            VehicleOwner bar = new VehicleOwner("bar", "056 ואת השאר תנחש");
+            //VehicleOwner bar = new VehicleOwner("bar", "056 ואת השאר תנחש");
             Console.WriteLine(bar.PhoneNumber + "\n" + bar.Name);
             Console.ReadLine();
 
@@ -28,7 +36,7 @@ namespace Ex03.ConsoleUI
 
 
             //// Engines TESTS
-            Engine fuelEngine = new FuelEngine(eFuelType.Octane95, 20, 100);
+            //Engine fuelEngine = new FuelEngine(eFuelType.Octane95, 20, 100);
             Console.WriteLine(fuelEngine.EnergyPercentage);
             Engine electricEngine = new ElectricEngine(30, 100);
             Console.WriteLine(electricEngine.EnergyPercentage);
