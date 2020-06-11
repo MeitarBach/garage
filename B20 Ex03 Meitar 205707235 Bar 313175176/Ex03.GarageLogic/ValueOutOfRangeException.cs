6d2d262b548d0c816474c4335be4bc05df79d4e6
MeitarBach <meitar.bach666@gmail.com>
2020-06-11 13:24:29 +0300

    initial commit

---
+++ B20 Ex03 Meitar 205707235 Bar 313175176/Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+    public class ValueOutOfRangeException : Exception
+    {
+        private readonly float r_MinValue;
+        private readonly float r_MaxValue;
+
+        public ValueOutOfRangeException(float i_SentValue, float i_MinValue, float i_MaxValue)
+            : base(string.Format("Invalid value: {0}. The value has to be in range: {1}-{2}",
+                i_SentValue, i_MinValue, i_MaxValue))
+        {
+            r_MinValue = i_MinValue;
+            r_MaxValue = i_MaxValue;
+        }
+
+        public float MinValue
+        {
+            get
+            {
+                return r_MinValue;
+            }
+        }
+
+        public float MaxValue
+        {
+            get
+            {
+                return r_MaxValue;
+            }
+        }
+    }
+}
---
+++ B20 Ex03 Meitar 205707235 Bar 313175176/Ex03.GarageLogic/Wheel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+    public class Wheel
+    {
+        private readonly string r_Manufacturer;
+        private float m_CurrentAirPressure;
+        private readonly float r_MaxAirPressure;
+        private const float k_MinAirPressure = 0;
+
+        public Wheel(string i_Manufacturer, float i_CurrentAirPressure, float i_MaxAirPressure)
+        {
+            r_Manufacturer = i_Manufacturer;
+            m_CurrentAirPressure = i_CurrentAirPressure;
+            r_MaxAirPressure = i_MaxAirPressure;
+        } 
+
+        public void InflateWheel(float i_AirPressureToAdd)
+        {
+            float newAirPressure = m_CurrentAirPressure + i_AirPressureToAdd;
+            
+            if (i_AirPressureToAdd < k_MinAirPressure || newAirPressure > r_MaxAirPressure)
+            {
+                throw new ValueOutOfRangeException(i_AirPressureToAdd, k_MinAirPressure, r_MaxAirPressure);
+            }
+            else
+            {
+                m_CurrentAirPressure += i_AirPressureToAdd;
+            }           
+        }
+    }
+}
